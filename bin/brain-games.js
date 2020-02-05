b4f692f2c38108683d1b9bd3c83eb8c83f0be032
Dmitriy Belyaev <dicibeel@gmail.com>
2020-02-05 21:59:53 +0200

    Turn 3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
+import readlineSync from '/home/a/frontend-project-lvl1/src/index.js';
 
 console.log("Welcome to the Brain Games!");
+
+const userName = readlineSync.question('May I have your name? ');
+
+console.log('Hello, ' + userName + '!');
