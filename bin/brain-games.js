5af90a24d74770844569f7556f4fb555f1b33d1b
Dmitriy Belyaev <dicibeel@gmail.com>
2020-02-10 23:04:27 +0200

    turn 3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from '/home/a/frontend-project-lvl1/src/index.js';
+import greeting from '../src/index.js';
 
-console.log("Welcome to the Brain Games!");
-
-const userName = readlineSync.question('May I have your name? ');
-
-console.log('Hello, ' + userName + '!');
+greeting();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+#!/usr/bin/env node
 import readlineSync from 'readline-sync';
+const greetings = () => {
 
-export default readlineSync;
+	console.log("Welcome to the Brain Games!");
+
+	const userName = readlineSync.question('May I have your name? ');
+
+	console.log('Hello, ' + userName + '!')
+};
+export default greetings;
