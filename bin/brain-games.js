778511d667ee257ab9bc08a82a8993422b5e9c70
Dmitriy Belyaev <dicibeel@gmail.com>
2020-02-12 00:00:56 +0200

    turn 4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import greeting from '../src/index.js';
+import greetings from '../src/index.js';
 
-greeting();
+greetings();
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
+
 const greetings = () => {
-
-	console.log("Welcome to the Brain Games!");
-
-	const userName = readlineSync.question('May I have your name? ');
-
-	console.log('Hello, ' + userName + '!')
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log('Hello, ' + userName + '!');
 };
 export default greetings;
