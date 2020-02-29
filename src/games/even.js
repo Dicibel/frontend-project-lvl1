50a4a56c2a8883432a4703c4deaa7373a7375f98
Dmitriy Belyaev <dicibeel@gmail.com>
2020-02-29 09:44:17 +0200

    fixed Git Actions

--- src/games/even.js
+++ src/games/even.js
@@ -1,12 +1,12 @@
 import randomNumber from '../lib/lib.js';
 import rulesOfGames from '../index.js';
 
-const isEvenNumber = (number) => (number % 2 === 0 ? 'yes' : 'no');
+const isEven = (number) => (number % 2 === 0 ? 'yes' : 'no');
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no"';
 const questionAndAnswer = () => {
   const question = randomNumber(0, 100);
-  const correctAnswer = isEvenNumber(question);
+  const correctAnswer = isEven(question);
   return [question, correctAnswer];
 };
 
